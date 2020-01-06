ee2bf5ae83d6a849d93fcf911ae9db2cd1941005
kraken000 <jiangyan2016@yeah.net>
2020-01-06 17:39:57 +0800

    add dataset

--- main.py
+++ main.py
@@ -12,3 +12,20 @@ import json
 import requests
 from tqdm import tqdm
 
+# print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal',
+               'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print('\nTrain_images.shape: {}, of {}'.format(train_images.shape, train_images.dtype))
+print('Test_images.shape: {}, of {}'.format(test_images.shape, test_images.dtype))
+
+train_images_gr = train_images.reshape(train_images.shape[0], 28, 28, 1)
+test_images_gr = test_images.reshape(test_images.shape[0], 28, 28, 1)
+
+print('\nTrain_images.shape: {}, of {}'.format(train_images_gr.shape, train_images_gr.dtype))
+print('Test_images.shape: {}, of {}'.format(test_images_gr.shape, test_images_gr.dtype))
+
