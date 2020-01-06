71d042640e0be11b0db1bbb690ca90d0a10e6657
kraken000 <jiangyan2016@yeah.net>
2020-01-06 17:18:08 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import subprocess
+import json
+import requests
+from tqdm import tqdm
+
